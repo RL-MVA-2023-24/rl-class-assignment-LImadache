c3b2e51973f976bb77c47c99fb08c2e5652a0797
LImadache <liamimadache@gmail.com>
2024-03-02 17:02:04 +0100

    Update dqn_agent.py

--- src/dqn_agent.py
+++ src/dqn_agent.py
@@ -9,25 +9,28 @@ from dqn_greedy_action import greedy_action
 
 class dqn_agent:
     def __init__(self, config, model):
-        device = "cuda" if next(model.parameters()).is_cuda else "cpu"
-        self.gamma = config['gamma']
-        self.batch_size = config['batch_size']
+        device = 'cpu'
         self.nb_actions = config['nb_actions']
-        self.memory = ReplayBuffer(config['buffer_size'],device)
-        self.epsilon_max = config['epsilon_max']
-        self.epsilon_min = config['epsilon_min']
-        self.epsilon_stop = config['epsilon_decay_period']
-        self.epsilon_delay = config['epsilon_delay_decay']
+        self.gamma = config['gamma'] if 'gamma' in config.keys() else 0.95
+        self.batch_size = config['batch_size'] if 'batch_size' in config.keys() else 100
+        buffer_size = config['buffer_size'] if 'buffer_size' in config.keys() else int(1e5)
+        self.memory = ReplayBuffer(buffer_size,device)
+        self.epsilon_max = config['epsilon_max'] if 'epsilon_max' in config.keys() else 1.
+        self.epsilon_min = config['epsilon_min'] if 'epsilon_min' in config.keys() else 0.01
+        self.epsilon_stop = config['epsilon_decay_period'] if 'epsilon_decay_period' in config.keys() else 1000
+        self.epsilon_delay = config['epsilon_delay_decay'] if 'epsilon_delay_decay' in config.keys() else 20
         self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
-        self.total_steps = 0
         self.model = model 
-        self.criterion = torch.nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=config['learning_rate'])
-        self.nb_gradient_steps = config['gradient_steps']
         self.target_model = deepcopy(self.model).to(device)
-        self.update_target_strategy = config['update_target_strategy']
-        self.update_target_freq = config['update_target_freq']
-        self.update_target_tau = config['update_target_tau']
+        self.criterion = config['criterion'] if 'criterion' in config.keys() else torch.nn.MSELoss()
+        lr = config['learning_rate'] if 'learning_rate' in config.keys() else 0.001
+        self.optimizer = config['optimizer'] if 'optimizer' in config.keys() else torch.optim.Adam(self.model.parameters(), lr=lr)
+        self.nb_gradient_steps = config['gradient_steps'] if 'gradient_steps' in config.keys() else 1
+        self.update_target_strategy = config['update_target_strategy'] if 'update_target_strategy' in config.keys() else 'replace'
+        self.update_target_freq = config['update_target_freq'] if 'update_target_freq' in config.keys() else 20
+        self.update_target_tau = config['update_target_tau'] if 'update_target_tau' in config.keys() else 0.005
+        self.monitoring_nb_trials = config['monitoring_nb_trials'] if 'monitoring_nb_trials' in config.keys() else 0
+
     
     def gradient_step(self):
         if len(self.memory) > self.batch_size:
@@ -40,8 +43,40 @@ class dqn_agent:
             loss.backward()
             self.optimizer.step() 
     
+    def MC_eval(self, env, nb_trials):   # NEW NEW NEW
+        MC_total_reward = []
+        MC_discounted_reward = []
+        for _ in range(nb_trials):
+            x,_ = env.reset()
+            done = False
+            trunc = False
+            total_reward = 0
+            discounted_reward = 0
+            step = 0
+            while not (done or trunc):
+                a = greedy_action(self.model, x)
+                y,r,done,trunc,_ = env.step(a)
+                x = y
+                total_reward += r
+                discounted_reward += self.gamma**step * r
+                step += 1
+            MC_total_reward.append(total_reward)
+            MC_discounted_reward.append(discounted_reward)
+        return np.mean(MC_discounted_reward), np.mean(MC_total_reward)
+    
+    def V_initial_state(self, env, nb_trials):   # NEW NEW NEW
+        with torch.no_grad():
+            for _ in range(nb_trials):
+                val = []
+                x,_ = env.reset()
+                val.append(self.model(torch.Tensor(x).unsqueeze(0).to(device)).max().item())
+        return np.mean(val)
+    
     def train(self, env, max_episode):
         episode_return = []
+        MC_avg_total_reward = []   
+        MC_avg_discounted_reward = []   
+        V_init_state = [] 
         episode = 0
         episode_cum_reward = 0
         state, _ = env.reset()
@@ -78,6 +113,21 @@ class dqn_agent:
             step += 1
             if done or trunc:
                 episode += 1
+                if self.monitoring_nb_trials>0:
+                    MC_dr, MC_tr = self.MC_eval(env, self.monitoring_nb_trials)    
+                    V0 = self.V_initial_state(env, self.monitoring_nb_trials)   
+                    MC_avg_total_reward.append(MC_tr)   
+                    MC_avg_discounted_reward.append(MC_dr)   
+                    V_init_state.append(V0)   
+                    episode_return.append(episode_cum_reward)   
+                    print("Episode ", '{:2d}'.format(episode), 
+                          ", epsilon ", '{:6.2f}'.format(epsilon), 
+                          ", batch size ", '{:4d}'.format(len(self.memory)), 
+                          ", ep return ", '{:4.1f}'.format(episode_cum_reward), 
+                          ", MC tot ", '{:6.2f}'.format(MC_tr),
+                          ", MC disc ", '{:6.2f}'.format(MC_dr),
+                          ", V0 ", '{:6.2f}'.format(V0),
+                          sep='')
                 print("Episode ", '{:3d}'.format(episode), 
                       ", epsilon ", '{:6.2f}'.format(epsilon), 
                       ", batch size ", '{:5d}'.format(len(self.memory)), 
